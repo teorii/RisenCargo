872bc0c82f15d28c0b0787d1cf27bbd145a8eb7c
snmetcalf <smetcalf@berkeley.edu>
2025-02-28 06:35:35 -0800

    ftl images

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3497f018c94e69fe28099cfca428d867.js"
+  "/precache-manifest.204e999caebe448926e87d878b097644.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,7 +10,7 @@ function Hero({ title, subtitle, buttons = false, primaryButton, secondaryButton
     textAlign: 'left',
     padding: '0 15%',
     backgroundSize: 'cover',
-    backgroundPosition: 'bottom',
+    backgroundPosition: 'center',
     color: 'white',
     backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${backgroundImage})`
   };
